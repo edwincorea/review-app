7130100426371eb26aaf7571d3aef9bc70eba145
Edwin Corea <Edwin Corea>
2017-02-04 08:10:01 -0600

    first commit

---
+++ review-king/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AddReviewPage } from '../pages/add-review-page/add-review-page';
+import { Reviews } from '../providers/reviews';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AddReviewPage
+  ],
+  providers: [Reviews]
+})
+export class AppModule {}
